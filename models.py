ddeeaca90e839ce63d4c4455150cf8c9d51254d1
Alec Lownes <alec.lownes@gmail.com>
2015-12-02 12:03:42 -0500

    Add data to model

--- models.py
+++ models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Vault(models.Model):
+    """
+    Keeps encrypted vault data by user
+    """
+    data = models.TextField()
+    owner = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.owner
